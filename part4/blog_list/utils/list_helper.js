4407389f15781020da1c4b8bbb9370c3e37cfbe3
Pekka Lehtikangas <plehtika@student.hive.fi>
2023-03-07 14:53:14 +0200

    Ex4.7 helper functions and unit tests finished

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,17 +2,58 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const likesReducer = (sum, blog) => {
+	return sum + blog.likes
+}
+
 const totalLikes = (blogs) => {
-	const reducer = (sum, blog) => {
-		return sum + blog.likes
-	}
+	return blogs.length
+		? blogs.reduce(likesReducer, 0)
+		: 0
+}
+
+const favoriteBlog = blogs => {
+	return blogs.length
+		? blogs.sort((a, b) => a.likes > b.likes ? -1 : 1)[0]
+		: {}
+}
+
+const mostBlogs = blogs => {
+	let allAuthors = []
+
+	blogs.forEach(filterBlog => {
+		const authorBlogs = blogs.filter(blog => blog.author === filterBlog.author)
+		allAuthors.push({
+			author: filterBlog.author,
+			blogs: authorBlogs.length
+		})
+	})
 
 	return blogs.length
-	? blogs.reduce(reducer, 0)
-	: 0
+	? allAuthors.sort((a, b) => a.blogs > b.blogs ? -1 : 1)[0]
+	: {}
+}
+
+const mostLikes = blogs => {
+	let authorLikes = []
+
+	blogs.forEach(filterBlog => {
+		const authorBlogs = blogs.filter(blog => blog.author === filterBlog.author)
+		authorLikes.push({
+			author: filterBlog.author,
+			likes: authorBlogs.reduce(likesReducer, 0)
+		})
+	})
+
+	return blogs.length
+	? authorLikes.sort((a, b) => a.blogs > b.blogs ? -1 : 1)[0]
+	: {}
 }
 
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes
 }

d03bca45a54dd9d7ab8f7c4d4da9c6bf5cc3a1cb
Pekka Lehtikangas <plehtika@student.hive.fi>
2023-03-07 15:02:29 +0200

    Ex4.7 helper functions and unit tests fix

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -22,32 +22,36 @@ const mostBlogs = blogs => {
 	let allAuthors = []
 
 	blogs.forEach(filterBlog => {
-		const authorBlogs = blogs.filter(blog => blog.author === filterBlog.author)
-		allAuthors.push({
-			author: filterBlog.author,
-			blogs: authorBlogs.length
-		})
+		if (!allAuthors.find(item => item.author === filterBlog.author)) {
+			const authorBlogs = blogs.filter(blog => blog.author === filterBlog.author)
+			allAuthors.push({
+				author: filterBlog.author,
+				blogs: authorBlogs.length
+			})
+		}
 	})
 
 	return blogs.length
-	? allAuthors.sort((a, b) => a.blogs > b.blogs ? -1 : 1)[0]
-	: {}
+		? allAuthors.sort((a, b) => a.blogs > b.blogs ? -1 : 1)[0]
+		: {}
 }
 
 const mostLikes = blogs => {
 	let authorLikes = []
 
 	blogs.forEach(filterBlog => {
-		const authorBlogs = blogs.filter(blog => blog.author === filterBlog.author)
-		authorLikes.push({
-			author: filterBlog.author,
-			likes: authorBlogs.reduce(likesReducer, 0)
-		})
+		if (!authorLikes.find(item => item.author === filterBlog.author)) {
+			const authorBlogs = blogs.filter(blog => blog.author === filterBlog.author)
+			authorLikes.push({
+				author: filterBlog.author,
+				likes: authorBlogs.reduce(likesReducer, 0)
+			})
+		}
 	})
 
 	return blogs.length
-	? authorLikes.sort((a, b) => a.blogs > b.blogs ? -1 : 1)[0]
-	: {}
+		? authorLikes.sort((a, b) => a.blogs > b.blogs ? -1 : 1)[0]
+		: {}
 }
 
 module.exports = {

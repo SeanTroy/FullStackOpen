1b02f812edc73cb99e1c32a2aaca16394a654ce4
Pekka Lehtikangas <plehtika@student.hive.fi>
2023-03-08 18:20:02 +0200

    Ex4.15 Bloglist expansion, step3

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -7,6 +7,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
@@ -21,6 +22,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

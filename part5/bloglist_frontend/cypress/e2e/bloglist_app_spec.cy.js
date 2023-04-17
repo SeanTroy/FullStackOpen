38abffffa00b99da3a8b12b6b3a346f11f8c9b4a
Pekka Lehtikangas <pekkalehtik@gmail.com>
2023-04-17 13:40:33 +0300

    Ex5.22 bloglist end to end testing, step6

--- part5/bloglist_frontend/cypress/e2e/bloglist_app_spec.cy.js
+++ part5/bloglist_frontend/cypress/e2e/bloglist_app_spec.cy.js
@@ -1,13 +1,13 @@
 describe('Blog app', function () {
 	beforeEach(function () {
-		cy.request('POST', 'http://localhost:3003/api/testing/reset')
+		cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
 		const user = {
 			name: 'Permanent User',
 			username: 'permanentuser',
 			password: 'permanent'
 		}
-		cy.request('POST', 'http://localhost:3003/api/users/', user)
-		cy.visit('http://localhost:3000')
+		cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user)
+		cy.visit('')
 	})
 
 	it('login form is shown', function () {
@@ -39,21 +39,24 @@ describe('Blog app', function () {
 
 	describe('when logged in', function () {
 		beforeEach(function () {
-			cy.get('#username').type('permanentuser')
-			cy.get('#password').type('permanent')
-			cy.get('#loginbutton').click()
+			cy.login({ username: 'permanentuser', password: 'permanent' })
+		})
+
+		it('a new blog can be created', function () {
 			cy.contains('new blog').click()
 			cy.get('input[name=title]').type('Test Title')
 			cy.get('input[name=author]').type('Test Author')
 			cy.get('input[name=url]').type('Test Url')
 			cy.contains('create').click()
-		})
-
-		it('a new blog can be created', function () {
 			cy.get('.blogentry').contains('Test Title by Test Author')
 		})
 
 		it('a blog can be liked', function () {
+			cy.createBlog({
+				title: 'Test Title',
+				author: 'Test Author',
+				url: 'Test Url'
+			})
 			cy.get('.blogentry').contains('view').click()
 			cy.get('.blogentry').contains('likes: 0')
 			cy.get('.blogentry').contains('like').click()
@@ -61,28 +64,34 @@ describe('Blog app', function () {
 		})
 
 		it('a blog can be deleted', function () {
-			cy.get('.blogentry').contains('Test Title by Test Author')
-			cy.contains('view').click()
-			cy.contains('remove').click()
+			cy.createBlog({
+				title: 'Test Title',
+				author: 'Test Author',
+				url: 'Test Url'
+			})
+			cy.get('.blogentry').contains('view').click()
+			cy.get('.blogentry').contains('remove').click()
 			cy.on('window:confirm', () => true) // cypress will auto accept confirmations anyway
 			cy.get('.blogentry').should('not.exist')
 		})
 
 		it('only the user who created a blog can see the delete button', function () {
+			cy.createBlog({
+				title: 'Test Title',
+				author: 'Test Author',
+				url: 'Test Url'
+			})
 			cy.get('#logoutbutton').click()
 			const testuser = {
 				name: 'Test User',
 				username: 'testuser',
 				password: 'testing'
 			}
-			cy.request('POST', 'http://localhost:3003/api/users/', testuser)
-			cy.get('#username').type('testuser')
-			cy.get('#password').type('testing')
-			cy.get('#loginbutton').click()
+			cy.request('POST', `${Cypress.env('BACKEND')}/users/`, testuser)
+			cy.login({ username: 'testuser', password: 'testing' })
 			cy.contains('view').click()
 			cy.contains('remove').should('not.exist')
 		})
-
 	})
 
 })
